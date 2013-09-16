9d795394a9bd4a905a0039a8ba094082c6f153c9
Aliaksei Katovich <aliaksei.katovich@gmail.com>
2013-09-16 22:13:34 +0300

    desktop-goodies: irssi: pass pid to script
    Pass pid to irssi-notify-msg script.
    
    Signed-off-by: Aliaksei Katovich <aliaksei.katovich@gmail.com>

--- desktop-goodies/irssi/scripts/notify.pl
+++ desktop-goodies/irssi/scripts/notify.pl
@@ -14,7 +14,8 @@ $VERSION = '0.0.1';
 
 sub pub_msg {
 	my ($server, $msg, $nick, $address, $target) = @_;
-	`irssi-notify-msg "$nick" "$address" >/dev/null 2>&1 &`;
+        my $ppid = getppid();
+	`irssi-notify-msg "$nick" "$address" "$ppid" >/dev/null 2>&1 &`;
 }
 
 Irssi::signal_add_last("message public", "pub_msg");
@@ -23,3 +24,4 @@ Irssi::signal_add_last("message own_public", "pub_msg");
 Irssi::signal_add_last("message own_private", "pub_msg");
 Irssi::signal_add_last("dcc request", "pub_msg");
 #Irssi::signal_add_last("print text", "pub_msg");
+Irssi::print("PID: " . getppid());
